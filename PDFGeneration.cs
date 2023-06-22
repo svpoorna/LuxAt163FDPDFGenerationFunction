5e8546d2dd0de6183f4fa1f9c54712ef9a9c1e22
poornachandra sadhu <pp@sadhus.onmicrosoft.com>
2023-06-22 09:20:43 +0530

    Final Commit after fixing calculated Field issue and added 190F PDF

--- PDFGeneration.cs
+++ PDFGeneration.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using Newtonsoft.Json.Linq;
 using iTextSharp.text.pdf;
+using static iTextSharp.text.pdf.codec.TiffWriter;
 
 
 namespace LuxAt163FDPDFGenerationFunction
@@ -13,9 +14,7 @@ namespace LuxAt163FDPDFGenerationFunction
             ResponseMessageModel responseMessageModel = new ResponseMessageModel();
             try
             {
-                //string result = "No Data No Results";
-
-                string jsondata = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+               string jsondata = Newtonsoft.Json.JsonConvert.SerializeObject(data);
 
 
                 using (MemoryStream outputPdfStream = new MemoryStream())
@@ -24,24 +23,20 @@ namespace LuxAt163FDPDFGenerationFunction
                     {
                         using (PdfStamper pdfStamper = new PdfStamper(pdfReader, outputPdfStream))
                         {
-                            AcroFields pdfFormFields = pdfStamper.AcroFields;
+                            var fieldtype=string.Empty;
+                            
+                            AcroFields pdfFormFields = pdfStamper.AcroFields;                           
+
+                            // Convert hidden calculated fileds to readable
+                            makeFieldsVisibile(pdfFormFields);                            
                             JObject json = JObject.Parse(jsondata);
                             foreach (var field in json)
-                            {
-                                //JTokenType fieldtype = field.Value.Type;
-                                string fieldName = field.Key;
-                                string fieldValue = field.Value.ToString();
-
-                                //  if (field.Key.Equals("JsonKey"))
-                                if (pdfFormFields.Fields.ContainsKey(fieldName))
-                                {
-                                    pdfFormFields.SetField(fieldName, fieldValue);
-                                }
-
-                            }
-                            //if the value is set to true, the PDF will be locked against further edits. 
-                            pdfStamper.FormFlattening = false;
-                            pdfStamper.Close();
+                              if (pdfFormFields.Fields.ContainsKey(field.Key))
+                                   pdfFormFields.SetField(field.Key, field.Value?.ToString());                                    
+                             
+                            //if the FormFlattening value is set to true, the PDF will be locked against further edits. 
+                            pdfStamper.FormFlattening = false;                      
+                            pdfStamper.Close(); 
 
                         }
                     }
@@ -58,7 +53,6 @@ namespace LuxAt163FDPDFGenerationFunction
 
                 }
 
-
             }
             catch (Exception ex)
             {
@@ -66,15 +60,27 @@ namespace LuxAt163FDPDFGenerationFunction
                 responseMessageModel.isRequestSuccess = false;
                 responseMessageModel.responseMessage = ex.Message.ToString();
                 return responseMessageModel;
-
             }
-
         }
 
+        private void makeFieldsVisibile(AcroFields pdfFormFields)
+        {
 
+            foreach (var fieldEntry in pdfFormFields.Fields)
+            {
+                string fieldName = fieldEntry.Key;
+                AcroFields.Item item = fieldEntry.Value;
 
+                if (fieldName.StartsWith("T"))
+                {
+                    pdfFormFields.SetFieldProperty(fieldName, "flags", PdfAnnotation.FLAGS_PRINT, null);
 
+                    pdfFormFields.SetFieldProperty(fieldName, "visibility", BaseField.VISIBLE, null);
+                }
 
+            }
+            //trow new NotImplementedException();
+        }
     }
 }
 
